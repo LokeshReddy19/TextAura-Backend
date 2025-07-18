284ebf2cf3fff61078365631e43605c0b1d82600
LokeshReddy19 <157341483+LokeshReddy19@users.noreply.github.com>
2025-07-19 02:46:54 +0530

    cors

--- main.py
+++ main.py
@@ -19,11 +19,15 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+
+
 
 # Request model
 class QueryModel(BaseModel):
